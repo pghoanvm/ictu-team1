16bb0bf4910da2ce156750bb1dec572cc8442489
Pfisthis2004 <159265292+Pfisthis2004@users.noreply.github.com>
2026-01-26 21:51:45 +0700

    Thêm danh mục ở trang chủ

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -45,7 +45,56 @@ export default function HomePage() {
         </div>
       </div>
 
-      {/* 2. DANH SÁCH SẢN PHẨM MỚI */}
+      {/* 2. DANH MỤC SẢN PHẨM */}
+      <div className="max-w-7xl mx-auto px-4 mb-16">
+        <h2 className="text-2xl font-bold uppercase text-center mb-8 tracking-wide">
+          Danh Mục Sản Phẩm
+        </h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {[
+            {
+              name: "Áo Khoác",
+              image:
+                "https://aothudong.com/upload/product/atd-129/ao-khoac-kaki-lot-long-nam-tinh.jpg",
+            },
+            {
+              name: "Quần Dài",
+              image:
+                "https://product.hstatic.net/200000690725/product/avt_web_1150_x_1475_px__9a19ababcc044b6890e913a22fe32c22_master.png",
+            },
+            {
+              name: "Áo Len",
+              image:
+                "https://4menshop.com/images/thumbs/2022/03/ao-len-co-tron-bo-soc-al005-mau-nau-16418.JPG",
+            },
+            {
+              name: "Phụ Kiện",
+              image:
+                "https://cdn.hstatic.net/products/200000690725/al011_63e4d647752348dfb69f2538e4e13982_master.png",
+            },
+          ].map((cat, index) => (
+            <Link
+              key={index}
+               to="shop?page=1"
+              className="relative group overflow-hidden cursor-pointer h-64 md:h-80"
+            >
+              <img
+                src={cat.image}
+                alt={cat.name}
+                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+              />
+              <div className="absolute inset-0 bg-black/20 group-hover:bg-black/40 transition"></div>
+              <div className="absolute bottom-4 left-0 right-0 text-center">
+                <span className="bg-white px-6 py-2 uppercase font-bold text-sm tracking-wider">
+                  {cat.name}
+                </span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      {/* 3. DANH SÁCH SẢN PHẨM MỚI */}
       <div className="max-w-7xl mx-auto px-4 mb-12">
         <div className="flex justify-between items-end mb-6 border-b pb-4">
           <h2 className="text-2xl font-bold uppercase tracking-wide">
